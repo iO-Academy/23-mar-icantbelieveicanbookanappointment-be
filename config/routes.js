96e826033f1c99c3b9049b2a3612b7429aa9ae56
Michael <michael.sullivan123@hotmail.co.uk>
2023-06-21 15:32:42 +0100

    Update

--- config/routes.js
+++ config/routes.js
@@ -4,7 +4,7 @@ const routes = (app) => {
   // * Get doctors
   app.get('/doctors', doctorSurgeryController.getDoctors)
   // * Get patient ID
-  app.post('/patient', doctorSurgeryController.getPatientId)
+  app.get('/patient', doctorSurgeryController.getPatientId)
   // * Get patient Name
   app.get('/patient/:patientId', doctorSurgeryController.getPatientName)
   // * Get appointments
